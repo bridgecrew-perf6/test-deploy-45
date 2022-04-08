2aed489e62dba5d0e2b06302a4780f6d371c40ce
juancr5 <juancr5@hotmail.com>
2022-04-08 17:00:36 -0500

    Update YML

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+describe('Prueba en el archivo demo.test.js', () => { 
+    
+    test('deben ser iguales los strings', () => { 
+        // 1 Inicializacion 
+        const mensaje = 'Hola Mundo'; 
+        // 2 Realizar estimulo 
+        const mensaje2 = 'Hola Mundo'; 
+        // 3 Observar el comportamiento 
+        expect(mensaje).toBe(mensaje2); 
+    }) 
+
+});
